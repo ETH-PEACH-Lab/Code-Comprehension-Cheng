74e1eef674c39d2f944f640028865a94e5f3c14e
xuancheng <xuanchengwudi@163.com>
2025-03-21 05:25:25 +0800

    Update README

---
+++ Data_Analysis_Behavioural_Data_LMM_Code/Element_Coverage_Analysis.R
@@ -0,0 +1,16 @@
+library(lme4)
+library(car)
+
+packageVersion("lme4") 
+packageVersion("car")  
+
+all_metrics_df <- read.csv("/Users/xuancheng/Desktop/master github/Code-Comprehension-Cheng/all_metrics_behavioural_data_summary.csv", sep=",")
+#print(head(all_metrics_df)) 
+
+glmm <- glmer(Comprehension_Result ~ Element_Coverage + Expertise + Complexity + Programming_Language + (1|Participant), 
+              data = all_metrics_df, 
+              family = binomial)
+
+print(summary(glmm))
+
+print(Anova(glmm))
---
+++ Data_Analysis_Behavioural_Data_LMM_Code/Execution_Order_Dynamic_Analysis.R
@@ -0,0 +1,14 @@
+library(lme4)
+library(car)
+
+packageVersion("lme4") 
+packageVersion("car")  
+
+all_metrics_df <- read.csv("/Users/xuancheng/Desktop/master github/Code-Comprehension-Cheng/all_metrics_behavioural_data_summary.csv", sep=",")
+#print(head(all_metrics_df)) 
+
+glmm = glmer(Comprehension_Result ~ Expertise + Execution_Order_Dynamic_Score + Complexity + Programming_Language + (1|Participant), data = all_metrics_df, family = binomial)
+
+print(summary(glmm))
+
+print(Anova(glmm))
